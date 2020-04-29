c5effbeae9c5b8601cc0e303b836438395c1a02f
Daniel Paz <danielpaz601@gmail.com>
2020-04-29 21:02:14 +0300

    Updates

---
+++ precache-manifest.4e0eabfd73d9f5a42da5050594bfc7f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1898766327d37fe432b79a9fa506452",
+    "url": "/react-covid-19-app/index.html"
+  },
+  {
+    "revision": "c47773d4644a70121bf9",
+    "url": "/react-covid-19-app/static/css/main.210f648a.chunk.css"
+  },
+  {
+    "revision": "2ab25373fd4816d03d44",
+    "url": "/react-covid-19-app/static/js/2.1e5fe838.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid-19-app/static/js/2.1e5fe838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c47773d4644a70121bf9",
+    "url": "/react-covid-19-app/static/js/main.1b26cb23.chunk.js"
+  },
+  {
+    "revision": "755980b49c234892231a",
+    "url": "/react-covid-19-app/static/js/runtime-main.d622c579.js"
+  },
+  {
+    "revision": "1a5f51c6741e52bf0c50680952a8f30c",
+    "url": "/react-covid-19-app/static/media/WorkSans-Thin.1a5f51c6.ttf"
+  },
+  {
+    "revision": "d98ae67e62212aae20d21d4f124c02ed",
+    "url": "/react-covid-19-app/static/media/coronavirus.d98ae67e.png"
+  }
+]);
